d11de0e36202943fa1285c17dad944487824d196
Agostina19 <agostina.blason19@gmail.com>
2022-05-30 18:59:30 -0300

    Se realizo ejercicio de operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -52,3 +52,34 @@ function mostrar_ocultar(valorMO){
     }
 
 }
+
+function calcularSuma(){
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].value= num1 + num2;
+}
+
+function calcularResta(){
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].value = num1 - num2;
+}
+
+function calcularMul(){
+    var num1, num2;
+
+    num1 = Number(document.getElementsByName("mul_num1")[0].value);
+    num2 = Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total" )[0].value = num1 * num2;
+}
+function calcularDiv(){
+    var num1, num2;
+
+    num1=Number(document.getElementsByName("div_num1")[0].value);
+    num2=Number(document.getElementsByName("div_num2")[0].value);
+    document.getElementsByName("div_total")[0].value= num1 / num2;
+}
